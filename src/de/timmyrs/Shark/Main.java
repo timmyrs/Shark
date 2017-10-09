29160a8bf9dbdb24801483a1fec31b592096aa0d
timmyRS <timmy14085@gmail.com>
2017-10-09 12:32:07 +0200

    Fix folder names containing . being trimmed

--- src/de/timmyrs/Shark/Main.java
+++ src/de/timmyrs/Shark/Main.java
@@ -523,9 +523,10 @@ public class Main
 			JsonObject fileObj = new JsonObject();
 			fileObj.addProperty("name", file.getName().split("\\.")[0]);
 			MessageDigest md = MessageDigest.getInstance("MD5");
-			String name = String.format("%032x", new BigInteger(1, md.digest(file.getName().split("\\.")[0].getBytes("UTF-8"))));
+			String name;
 			if(file.isDirectory())
 			{
+				name = String.format("%032x", new BigInteger(1, md.digest(file.getName().getBytes("UTF-8"))));
 				fileObj.addProperty("type", "/dir");
 				File outFile = new File(outputFile.getPath() + "/" + name);
 				if((outFile.exists() && outFile.isDirectory()) || outFile.mkdir())
@@ -547,6 +548,7 @@ public class Main
 			}
 			else
 			{
+				name = String.format("%032x", new BigInteger(1, md.digest(file.getName().split("\\.")[0].getBytes("UTF-8"))));
 				File outFile = new File(outputFile.getPath() + "/" + name + ".bin");
 				if(!outFile.exists())
 				{
